e61715a5a9bbf95a22bb363fd1026ae1d707e2e8
renjith85 <renjith85@gmail.com>
2023-04-17 14:29:02 -0700

    add missing keys to financroo tpp client application

--- openbanking/deploy/setup-ce.sh
+++ openbanking/deploy/setup-ce.sh
@@ -580,10 +580,26 @@ function create_client_financroo {
             {
                 "use": "sig",
                 "kty": "RSA",
+                "alg": "PS256",
+                "n": "j0Rh-pDbmdaVr0nIaHz6uXUOfRYcp2L2xM5QeAIw5u0hgjd8-xlfHllBkbce0DIwDCqNXozkqqs709PodzGHHR_v4eCE8yjix_uRe5ckK4s9JLTyE075XXGUC5439X0lHvyXo7vFIaFgbVyQs3XSkCLDyi2fs62hNpfkUKuIkUJ-dUz1SZ8YKn-FS1d93PttVXItEv0sSRUGw_pvIBtojeS_xFirxiCITscLidxJXOeUXHhqYNG5WrnQI5q-zPZP_PFKL1Mwm_ER86km71WhzNiw33NL3RjsodKrd25IBNiq4IGWgSrDcegSSeotEsOyGs70qSXh2RkpZ1EPoMYhSw",
+                "e": "AQAB"
+            },
+            {
+                "use": "sig",
+                "kty": "RSA",
                 "kid": "167467200346518873990055631921812347975180003245",
                 "alg": "RS256",
                 "n": "4b_IX1bV29pw6_Ce8DdkoNx4dxJnDD9AyxmTG2z99cvlHG6BJaMF6l09ncGXGbv3dufDKrhftkwfbTBdpUEAeext_ugCmXTV06Fayva6Iq7xCNE8pA6hJT1y3Edsqq3IU8KVivYjYwd_vrSUfCe8pQRsR6K8rqnJ66ryn0yewkTEyCgPIv6pOMbgq1d5iX_2G9rZNhj74miN5y4fy0tsbI3q2RUOzt2d-htkoysqu3Xta6qPA3vEJ2FnQo3dhgw4XSCEvjz-HSGnsTC-XBv6j6jI9SD5jI2UYqnyDcYmRHPJx2sQ_c8aLYHRdZxrxqIxUzulS6g0x74E2m0gBMKF5w",
                 "e": "AQAB"
+            },
+            {
+                "use": "sig",
+                "kty": "EC",
+                "kid": "Y4jgLf37RRxrv3B904cnCb8a_oeGz81OgOxSArzLaII",
+                "crv": "P-256",
+                "alg": "ES256",
+                "x": "O3kS63BWBDcXc--UqpRGFctBIDPgIlU5u5wCZlK4gH0",
+                "y": "D3cnnEZTW668_9OA27WUQHucgDZTG5xu2N0z_bZQwqA"
             }
         ]
     },
@@ -899,10 +915,26 @@ function update_financroo_redirect_url {
             {
                 "use": "sig",
                 "kty": "RSA",
+                "alg": "PS256",
+                "n": "j0Rh-pDbmdaVr0nIaHz6uXUOfRYcp2L2xM5QeAIw5u0hgjd8-xlfHllBkbce0DIwDCqNXozkqqs709PodzGHHR_v4eCE8yjix_uRe5ckK4s9JLTyE075XXGUC5439X0lHvyXo7vFIaFgbVyQs3XSkCLDyi2fs62hNpfkUKuIkUJ-dUz1SZ8YKn-FS1d93PttVXItEv0sSRUGw_pvIBtojeS_xFirxiCITscLidxJXOeUXHhqYNG5WrnQI5q-zPZP_PFKL1Mwm_ER86km71WhzNiw33NL3RjsodKrd25IBNiq4IGWgSrDcegSSeotEsOyGs70qSXh2RkpZ1EPoMYhSw",
+                "e": "AQAB"
+            },
+            {
+                "use": "sig",
+                "kty": "RSA",
                 "kid": "167467200346518873990055631921812347975180003245",
                 "alg": "RS256",
                 "n": "4b_IX1bV29pw6_Ce8DdkoNx4dxJnDD9AyxmTG2z99cvlHG6BJaMF6l09ncGXGbv3dufDKrhftkwfbTBdpUEAeext_ugCmXTV06Fayva6Iq7xCNE8pA6hJT1y3Edsqq3IU8KVivYjYwd_vrSUfCe8pQRsR6K8rqnJ66ryn0yewkTEyCgPIv6pOMbgq1d5iX_2G9rZNhj74miN5y4fy0tsbI3q2RUOzt2d-htkoysqu3Xta6qPA3vEJ2FnQo3dhgw4XSCEvjz-HSGnsTC-XBv6j6jI9SD5jI2UYqnyDcYmRHPJx2sQ_c8aLYHRdZxrxqIxUzulS6g0x74E2m0gBMKF5w",
                 "e": "AQAB"
+            },
+            {
+                "use": "sig",
+                "kty": "EC",
+                "kid": "Y4jgLf37RRxrv3B904cnCb8a_oeGz81OgOxSArzLaII",
+                "crv": "P-256",
+                "alg": "ES256",
+                "x": "O3kS63BWBDcXc--UqpRGFctBIDPgIlU5u5wCZlK4gH0",
+                "y": "D3cnnEZTW668_9OA27WUQHucgDZTG5xu2N0z_bZQwqA"
             }
         ]
     },
